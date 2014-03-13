950fbb982898c61e4f492a6bd880a650cc4dd4f7
David Alméciga <wdavid@dagrinchi.com>
2014-03-13 12:47:09 -0500

    Fix android

--- platforms/android/bin/APC-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/david/phonegap-projects/APC-Mapps/platforms/android/bin/APC-debug-unaligned.apk : \
-/Users/david/phonegap-projects/APC-Mapps/platforms/android/bin/APC.ap_ \
-/Users/david/phonegap-projects/APC-Mapps/platforms/android/bin/classes.dex \
-/Users/david/phonegap-projects/APC-Mapps/platforms/android/CordovaLib/bin/classes.jar \
